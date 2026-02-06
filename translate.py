49f03287a64c259da916541f4af6adb22f3ec883
mdiug53-creator <mdiug53@gmail.com>
2026-02-06 23:43:57 +0100

    Update translate.py with corrected indentation for Python 3.14

--- translate.py
+++ translate.py
@@ -1,24 +1,10 @@
 import requests
 
-class Translator:
-    def __init__(self, source_language, target_language):
-        self.source_language = source_language
-        self.target_language = target_language
-        self.api_url = 'https://libretranslate.com/translate'
+res = requests.post("https://libretranslate.de/translate", data={
+    "q": "Cześć, jak się masz?",
+    "source": "pl",
+    "target": "en",
+    "format": "text"
+}).json()
 
-    def translate(self, text):
-        try:
-            response = requests.post(
-                self.api_url,
-                data={
-                    'q': text,
-                    'source': self.source_language,
-                    'target': self.target_language,
-                    'format': 'text'
-                }
-            )
-            response.raise_for_status()
-            return response.json()['translatedText']
-        except requests.exceptions.RequestException as e:
-            print(f'Error occurred: {e}')  
-            return None
+print(res)
